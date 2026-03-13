0e15d68eff663d0d25836221ce26f7c01b9ac09c
Anca <moroianca22@stud.ase.ro>
2026-03-13 21:23:15 +0200

    commit

--- Masini.c
+++ Masini.c
@@ -1,131 +1,159 @@
 #define _CRT_SECURE_NO_WARNINGS
-#include<string.h>
 #include<malloc.h>
+#include<stdlib.h>
+#include<string.h>
 #include<stdio.h>
-#include <stdlib.h>
 
 
 typedef struct Masina Masina;
+typedef struct Nod Nod;
+typedef struct ListaDubla ListaDubla;
 
 struct Masina {
-	int nrUsi;
-	float pret;
+	int id;
 	char* culoare;
-
+	float pret;
+	char serie;
 };
 
-struct Masina initializareMasina(int nrUsi, float pret, const char* culoare) {
+struct Masina initializaMasina(int id, float pret, char serie, const char* culoare) {
 	struct Masina m;
-	m.nrUsi = nrUsi;
+	m.id = id;
 	m.pret = pret;
-	m.culoare = (char*)malloc(sizeof(char)*(strlen(culoare) + 1));
+	m.serie = serie;
+	m.culoare = (char*)malloc(sizeof(char) * strlen(culoare) + 1);
 	strcpy_s(m.culoare, strlen(culoare) + 1, culoare);
-
 	return m;
 }
 
-
-void afisareMasina(struct Masina m) {
-	printf("\nNrUsi:%d", m.nrUsi);
+void afiseazaMasina(struct Masina m) {
+	printf("\nId:%d",m.id);
 	printf("\nPret:%.2f", m.pret);
+	printf("\nSerie:%c", m.serie);
 	printf("\nCuloare:%s", m.culoare);
 	printf("\n");
 }
 
-void afisareVector(struct Masina* masina,int nrElemente) {
+Masina copiazaMasina(struct Masina m) {
+	return initializaMasina(m.id, m.pret, m.serie, m.culoare);
+}
+
+void afiseazaVector(Masina* m, int nrElemente) {
 	for (int i = 0; i < nrElemente; i++) {
-		afisareMasina(*(masina +i));
+		afiseazaMasina(*(m + i));
 	}
 }
 
-void dezalocareVector(struct Masina** m, int* nrElemente) {
-	for (int i = 0; i < *nrElemente; i++) {
-		free((*m)[i].culoare);
-	}
-	free(*m);
-	*m = NULL;
-	*nrElemente = 0;
-}
-
-Masina copiazaMasina( Masina m) {
-	return initializareMasina(m.nrUsi, m.pret, m.culoare);
-}
-
-void copiazaAnumiteElemente(struct Masina* m, float prag, int nrElemente, struct Masina** vectorNou,int *dimensiune) {
+void afiseazaCuPrag(struct Masina* m, int nrElemente, float prag, Masina** vectorNou,int *dimensiune) {
 	*dimensiune = 0;
 	for (int i = 0; i < nrElemente; i++) {
 		if (m[i].pret < prag) {
 			(*dimensiune)++;
 		}
 	}
+
 	*vectorNou = (Masina*)malloc(sizeof(Masina) * (*dimensiune));
 	*dimensiune = 0;
 	for (int i = 0; i < nrElemente; i++) {
 		if (m[i].pret < prag) {
 			(*vectorNou)[*dimensiune] = copiazaMasina(m[i]);
 			(*dimensiune)++;
-		}	
+		}
 	}
 }
 
-//citire vector fisier
-
-Masina citireMasinaDinFisier(FILE* file) {
+Masina citireFisier(FILE* f) {
 	char buffer[100];
-	char sep[3] = "\n,";
-	fgets(buffer, 100, file);
+	char sep[3] = ",\n";
+	fgets(buffer, 100, f);
+	char *aux;
 	Masina m1;
-	char* aux;
 	aux = strtok(buffer, sep);
-	m1.nrUsi = atoi(aux);
+	m1.id = atoi(aux);
 	aux = strtok(NULL, sep);
 	m1.pret = atof(aux);
 	aux = strtok(NULL, sep);
+	m1.serie = aux[0];
+	aux = strtok(NULL, sep);
 	m1.culoare = malloc(strlen(aux) + 1);
 	strcpy_s(m1.culoare, strlen(aux) + 1, aux);
-
 	return m1;
+	}
 
+void dezalocareVector(struct Masina** m, int *nrElemente) {
+	for (int i = 0; i < *nrElemente; i++) {
+		free((*m)[i].culoare);
+	}
+	free(*m);
+	*nrElemente = 0;
+	*m = NULL;
+}
+
+struct Nod {
+	Masina info;
+	Nod* next;
+	Nod* prev;
+};
+
+struct ListaDubla {
+	Nod* prim;
+	Nod* ultim;
+};
+
+void afiseazaLD(struct ListaDubla lista) {
+	Nod* p = lista.prim;
+	while (p) {
+		afiseazaMasina(p->info);
+		p = p->next;
+	}
+}
+
+void adaugaLd(ListaDubla* ld, Masina masinaLD) {
+	Nod* nou = malloc(sizeof(Masina));
+	nou->info = masinaLD;
+	nou->next = nou->prev = NULL;
+	if (ld->ultim) {
+		nou->prev = ld->ultim;
+		ld->ultim->next = nou;
+		ld->ultim = nou;
+	}
+	else {
+		ld->ultim = ld->prim = nou;
+	}
 }
 
 
 int main() {
 
-	struct Masina m1;
-	m1 = initializareMasina(2, 2500, "alb");
-	afisareMasina(m1);
+	struct Masina m = initializaMasina(1, 4500.00, 'S', "alb");
+	afiseazaMasina(m);
 
 	int nrMasini = 3;
-	Masina* mV = (Masina*)malloc(sizeof(Masina)*nrMasini);
-	mV[0] = initializareMasina(2, 4500.22, "neagra");
-	mV[1] = initializareMasina(3, 8500.00, "gri");
-	mV[2] = initializareMasina(4, 3500.00, "verde");
-
+	Masina* vectorMasini = (Masina*)malloc(sizeof(Masina) * (nrMasini));
+	vectorMasini[0] = initializaMasina(1, 4500.00, 'S', "alb");
+	vectorMasini[1] = initializaMasina(2, 5500.00, 'a', "roz");
+	vectorMasini[2] = initializaMasina(3, 6500.00, 'b', "verde");
 	printf("\nAfisare vector");
-	afisareVector(mV, nrMasini);
+	afiseazaVector(vectorMasini, nrMasini);
 
-	Masina* vectorNou = NULL;
-	int dim = 0;
-	copiazaAnumiteElemente(mV, 5000, nrMasini, &vectorNou, &dim);
-	printf("\n Vector filtrat");
-	afisareVector(vectorNou, dim);
-	dezalocareVector(&vectorNou, &dim);
 
-	printf("\nCitire din fisier\n");
+	int dimensiune = 0;
+	Masina* masinaVector = NULL;
+	afiseazaCuPrag(vectorMasini, nrMasini, 5500.00, &masinaVector, &dimensiune);
+	printf("\nAfisare vector cu prag");
+	afiseazaVector(masinaVector, dimensiune);
 
 	FILE* f = fopen("masini.txt", "r");
-	Masina mFisier;
+	Masina masiniF;
+	
 
+	printf("\nAfisare MASINI fisier");
 	for (int i = 0; i < 3; i++) {
-		mFisier = citireMasinaDinFisier(f);
-		afisareMasina(mFisier);
-		free(mFisier.culoare);
+		masiniF = citireFisier(f);
+		afiseazaMasina(masiniF);
+
 	}
-
 	fclose(f);
-
-
-
 
 	return 0;
 }
